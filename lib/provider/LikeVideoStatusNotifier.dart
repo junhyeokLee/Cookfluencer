d4af9ae1c871a13fbb6e5164bcd4a9e926706f71
lee junhyeok <junhyeoklee@sports2i.com>
2024-10-02 16:27:47 +0900

    ios 실행 안되던 문제 해결 , 비디오 좋아요 기능, 비디오 디테일 화면 생성

---
+++ lib/provider/LikeVideoStatusNotifier.dart
@@ -0,0 +1,99 @@
+import 'package:cookfluencer/data/videoData.dart';
+import 'package:cookfluencer/sharedPreferences/sharedPreferences.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:cookfluencer/data/channelData.dart';
+
+// LikeStatus 객체 정의
+class LikeChannelStatus {
+  final String id; // 채널 ID
+  final String channelName; // 채널 이름
+  final String channelDescription; // 채널 설명
+  final String channelUrl; // 채널 URL
+  final String thumbnailUrl; // 썸네일 URL
+  final int subscriberCount; // 구독자 수
+  final int videoCount; // 비디오 수
+  final List<VideoData> videos; // 비디오 리스트
+  final String section; // 섹션
+  final bool isLiked; // 좋아요 여부
+
+  LikeChannelStatus({
+    required this.id,
+    required this.channelName,
+    required this.channelDescription,
+    required this.channelUrl,
+    required this.thumbnailUrl,
+    required this.subscriberCount,
+    required this.videoCount,
+    required this.videos,
+    required this.section,
+    required this.isLiked,
+  });
+}
+
+// LikeStatusNotifier 정의
+class LikeChannelStatusNotifier extends StateNotifier<Map<String, LikeChannelStatus>> {
+  LikeChannelStatusNotifier() : super({}) {
+    _loadLikedChannels(); // 앱 시작 시 좋아요된 채널 로드
+  }
+
+  // 저장된 좋아요 상태를 로드하는 메소드
+  Future<void> _loadLikedChannels() async {
+    final likedChannels = await loadLikedChannelData(); // 기기에서 좋아요된 채널 불러오기
+
+    // 각 채널의 좋아요 상태를 업데이트
+    for (ChannelData channelData in likedChannels) {
+      state[channelData.id] = LikeChannelStatus(
+        id: channelData.id,
+        channelName: channelData.channelName,
+        channelDescription: channelData.channelDescription,
+        channelUrl: channelData.channelUrl,
+        thumbnailUrl: channelData.thumbnailUrl,
+        subscriberCount: channelData.subscriberCount,
+        videoCount: channelData.videoCount,
+        videos: channelData.videos,
+        section: channelData.section,
+        isLiked: true,
+      );
+    }
+  }
+
+  void toggleLike(ChannelData channelData) async {
+    final isLiked = state[channelData.id]?.isLiked ?? false;
+
+    // 상태 업데이트
+    state = {
+      ...state,
+      channelData.id: LikeChannelStatus(
+        id: channelData.id,
+        channelName: channelData.channelName,
+        channelDescription: channelData.channelDescription,
+        channelUrl: channelData.channelUrl,
+        thumbnailUrl: channelData.thumbnailUrl,
+        subscriberCount: channelData.subscriberCount,
+        videoCount: channelData.videoCount,
+        videos: channelData.videos,
+        section: channelData.section,
+        isLiked: !isLiked, // 좋아요 상태 반전
+      )
+    };
+
+    // 기기에 저장 또는 삭제
+    if (!isLiked) {
+      await saveChannelData(channelData); // 좋아요 추가 시 저장
+    } else {
+      await removeChannelData(channelData.id); // 좋아요 해제 시 삭제
+    }
+
+    // 상태 변경 후 UI 갱신
+    state = {...state};  // 강제로 상태를 다시 설정하여 UI가 업데이트되도록 함
+
+  }
+}
+
+// LikeStatusNotifier Provider 정의
+  final likeStatusProvider = StateNotifierProvider<LikeChannelStatusNotifier,
+      Map<String, LikeChannelStatus>>((ref) {
+    return LikeChannelStatusNotifier();
+  });
+
---
+++ lib/ui/screen/VideoDetailScreen.dart
@@ -0,0 +1,178 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cookfluencer/common/CircularLoading.dart';
+import 'package:cookfluencer/common/EmptyMessage.dart';
+import 'package:cookfluencer/common/ErrorMessage.dart';
+import 'package:cookfluencer/common/util/ScreenUtil.dart';
+import 'package:cookfluencer/data/channelData.dart';
+import 'package:cookfluencer/data/videoData.dart';
+import 'package:cookfluencer/provider/ChannelProvider.dart';
+import 'package:cookfluencer/ui/widget/common/ChannelItemHorizontal.dart';
+import 'package:cookfluencer/ui/widget/common/FilterRecipe.dart';
+import 'package:cookfluencer/ui/widget/common/VideoItem.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+
+class ChannelDetailScreen extends HookConsumerWidget {
+  final ChannelData channelData;
+
+  const ChannelDetailScreen({
+    Key? key,
+    required this.channelData,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final selectedFilter = useState<FilterOption>(FilterOption.viewCount);
+    final showFilterOptions = useState<bool>(false);
+    final pagingController =
+        useState(PagingController<int, QueryDocumentSnapshot>(
+      firstPageKey: 0,
+    ));
+
+    // 비디오 리스트 데이터 가져오기
+    Future<void> fetchVideos(int pageKey) async {
+      try {
+        final searchParams = {
+          'channel_id': channelData.id,
+          'filter': selectedFilter.value,
+          'start_after':
+              pageKey == 0 ? null : pagingController.value.itemList!.last,
+        };
+
+        await Future.delayed(Duration(milliseconds: 500)); // 500ms 지연
+
+        final newVideos =
+            await ref.read(videosByChannelProvider(searchParams).future);
+
+        final isLastPage = newVideos.isEmpty;
+        if (isLastPage) {
+          pagingController.value.appendLastPage(newVideos);
+        } else {
+          final nextPageKey = pageKey + newVideos.length;
+          pagingController.value.appendPage(newVideos, nextPageKey);
+        }
+      } catch (error) {
+        pagingController.value.error = error; // 오류 발생 시 처리
+      }
+    }
+
+    // 필터 변경 시, PagingController를 새로 생성
+    useEffect(() {
+      // PagingController 초기화
+      pagingController.value = PagingController<int, QueryDocumentSnapshot>(
+        firstPageKey: 0,
+      );
+      // 필터가 변경되면 새로운 페이지 요청
+      fetchVideos(0); // 이제 이 호출이 올바르게 작동합니다.
+      return null; // clean up 함수 반환
+    }, [selectedFilter.value]);
+
+    useEffect(() {
+      // PagingController에 페이지 요청 리스너 추가
+      pagingController.value.addPageRequestListener(fetchVideos);
+      return () =>
+          pagingController.value.removePageRequestListener(fetchVideos);
+    }, [pagingController.value]);
+
+    return Scaffold(
+      appBar: AppBar(
+        titleSpacing: 0,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 16),
+          child: IconButton(
+            icon: Icon(Icons.arrow_back_ios),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ),
+        title: Text(
+          channelData.channelName,
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 16, top: 24),
+                  child: Text('인플루언서',
+                      style: Theme.of(context).textTheme.labelLarge),
+                ),
+                ChannelItemHorizontal(channelData: channelData),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 16, top: 12, bottom: 12, right: 20),
+                  child: Text('레시피 영상',
+                      style: Theme.of(context).textTheme.labelLarge),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 12, right: 20),
+                  child: FilterRecipe(
+                    selectedFilter: selectedFilter,
+                    showFilterOptions: showFilterOptions,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.only(left: 16, right: 16),
+            sliver: PagedSliverList<int, QueryDocumentSnapshot>(
+              pagingController: pagingController.value,
+              builderDelegate: PagedChildBuilderDelegate<QueryDocumentSnapshot>(
+                itemBuilder: (context, videoSnapshot, index) {
+                  final video = videoSnapshot.data() as Map<String, dynamic>;
+                  final videoData = VideoData(
+                    id: video['id'] ?? 'Unknown',
+                    channelId: video['channel_id'] ?? 'Unknown',
+                    channelName: video['channel_name'] ?? 'Unknown',
+                    description: video['description'] ?? '',
+                    thumbnailUrl: video['thumbnail_url'] ?? '',
+                    title: video['title'] ?? 'Unknown',
+                    uploadDate: video['upload_date'] ?? '',
+                    videoId: video['video_id'] ?? '',
+                    videoUrl: video['video_url'] ?? '',
+                    viewCount:
+                        int.tryParse(video['view_count'].toString()) ?? 0,
+                    section: video['section'] ?? '',
+                  );
+                  return VideoItem(
+                    video: videoData,
+                    size: ScreenUtil.width(context, 0.2),
+                    titleWidth: ScreenUtil.width(context, 0.65),
+                    channelWidth: ScreenUtil.width(context, 0.35),
+                    onVideoItemClick: () {},
+                  );
+                },
+                firstPageProgressIndicatorBuilder: (context) =>
+                    Center(child: CircularLoading()),
+                newPageProgressIndicatorBuilder: (context) =>
+                    Center(child: CircularLoading()),
+                noItemsFoundIndicatorBuilder: (context) =>
+                    EmptyMessage(message: '비디오가 없음'),
+                noMoreItemsIndicatorBuilder: (context) =>
+                    EmptyMessage(message: '더 이상 비디오 없음'),
+              ),
+            ),
+          ),
+          // 오류 발생 시 메시지 표시
+          if (pagingController.value.error != null)
+            SliverToBoxAdapter(
+              child: ErrorMessage(
+                  message: '오류 발생: ${pagingController.value.error}'),
+            ),
+        ],
+      ),
+    );
+  }
+}
